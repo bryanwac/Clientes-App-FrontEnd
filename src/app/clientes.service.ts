97aa772d313d3ed949e3d26883da17821fc8aa2e
bryan <bryanwac@gmail.com>
2022-10-07 12:41:51 -0300

    Listando os clientes através da API

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -10,18 +10,14 @@ import { Observable } from 'rxjs';
 })
 export class ClientesService {
 
-  constructor( private http: HttpClient ) { 
-
-  }
+  constructor( private http: HttpClient ) { }
 
   salvar( cliente: Cliente ) : Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes' , cliente)
   }
 
-  getCliente() : Cliente {
-    let cliente : Cliente = new Cliente();
-    cliente.nome = 'Fulano de tal';
-    cliente.cpf = '88888888888';
-    return cliente;
+  getClientes(): Observable<Cliente[]>{
+    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
   }
+  
 }
